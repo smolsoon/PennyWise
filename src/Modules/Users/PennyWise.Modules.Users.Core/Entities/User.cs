e0d0c00142bc75da83054ec19381dc84cf4016fd
Kamil Smolik <smolik.k@eplan.pl>
2024-11-22 19:34:59 +0100

    First commit  - Create projects

---
+++ src/Modules/Users/PennyWise.Modules.Users.Core/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace PennyWise.Modules.Users.Core.Entities;
+
+internal class User
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+    public string RoleId { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ src/Modules/Users/PennyWise.Modules.Users.Core/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PennyWise.Modules.Users.Core;
+
+internal static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        
+        return services;
+    }
+}
